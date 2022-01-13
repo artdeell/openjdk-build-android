7924896fe4f8df89da08bf066c9ef07d05db0ebe
Google61 <golubev634@gmail.com>
2022-01-13 07:56:30 +0300

    Update buildlibs.sh

--- buildlibs.sh
+++ buildlibs.sh
@@ -4,8 +4,8 @@ set -e
 
 if [ "$BUILD_IOS" != "1" ] && [ "$API" == "19" ]; then
 cd api19
-$CC --sysroot=$TOOLCHAIN/sysroot $CPPFLAGS *.c
-$CXX --sysroot=$TOOLCHAIN/sysroot $CPPFLAGS -std=c++11 *.cpp
+$CC --sysroot=$TOOLCHAIN $CPPFLAGS $LDFLAGS *.c
+$CXX --sysroot=$TOOLCHAIN $CPPFLAGS $LDFLAGS -std=c++11 *.cpp
 find . | grep *.o
 fi
 
