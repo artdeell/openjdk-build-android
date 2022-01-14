7534be341ab681913179cb5d3fa2932165b7b1d0
Google61 <golubev634@gmail.com>
2022-01-14 08:53:11 +0300

    Update uchar.h

--- api19/include/uchar.h
+++ api19/include/uchar.h
@@ -34,13 +34,7 @@ __BEGIN_DECLS
 #define __STD_UTF_32__ 1
 //size_t c16rtomb(char* __restrict, char16_t, mbstate_t* __restrict);
 size_t c32rtomb(char* __restrict, char32_t, mbstate_t* __restrict);
-//size_t mbrtoc16(char16_t* __restrict,
-                const char* __restrict,
-                size_t,
-                mbstate_t* __restrict);
-size_t mbrtoc32(char32_t* __restrict,
-                const char* __restrict,
-                size_t,
-                mbstate_t* __restrict);
+//size_t mbrtoc16(char16_t* __restrict, const char* __restrict, size_t, mbstate_t* __restrict);
+size_t mbrtoc32(char32_t* __restrict, const char* __restrict, size_t, mbstate_t* __restrict);
 __END_DECLS
 #endif /* _UCHAR_H_ */
