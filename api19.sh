abe5fcce1b547f6f78ba41c2000287483816ceea
Google61 <golubev634@gmail.com>
2022-01-14 07:05:49 +0300

    Create api19.sh

---
+++ api19.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+. setdevkitpath.sh
+API19=$GITHUB_WORKSPACE/api19
+if [ "$BUILD_IOS" != "1" ] && [ "$API" == "19" ]; then
+cd $API19
+$CC -c -I$API19/include *.c
+$CXX -c -I$API19/include *.cpp
+find . | grep *.o
+fi
