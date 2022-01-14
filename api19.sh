6bdab9c8c63cac81b2371b25dd7d4f40efc93610
Google61 <golubev634@gmail.com>
2022-01-14 12:49:36 +0300

    Update api19.sh

--- api19.sh
+++ api19.sh
@@ -4,7 +4,8 @@ set -e
 API19="$(pwd)/api19"
 if [ "$BUILD_IOS" != "1" ] && [ "$API" == "19" ]; then
 cd $API19
-$CC -c -I$API19/include *.c
-$CXX -c -I$API19/include -std=c++11 *.cpp
-find . | grep *.o
+$CC -v -c -I$API19/include *.c
+$CXX -v -c -I$API19/include -std=c++11 *.cpp
+find .
 fi
+exit 1
