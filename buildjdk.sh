09209211177afbc31035c01913d115d3eab503fe
Google61 <golubev634@gmail.com>
2022-01-16 18:46:51 +0300

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -88,7 +88,7 @@ if [ "$error_code" -ne 0 ]; then
 fi
 
 cd build/${JVM_PLATFORM}-${TARGET_JDK}-${JVM_VARIANTS}-${JDK_DEBUG_LEVEL}
-make JOBS=4 images >/dev/null || cat build/${JVM_PLATFORM}-${TARGET_JDK}-${JVM_VARIANTS}-${JDK_DEBUG_LEVEL}/make-support/failure-logs/*.log && exit 0
+make JOBS=4 images
 
 #if [ "$error_code" -ne 0 ]; then
 #  echo "Build failure, exited with code $error_code. Trying again."
