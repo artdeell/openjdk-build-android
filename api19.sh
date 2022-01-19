1aa3789ac6cdd547af39c96e0a6551fa3dcc9fbf
Google61 <golubev634@gmail.com>
2022-01-19 22:28:25 +0300

    Update api19.sh

--- api19.sh
+++ api19.sh
@@ -2,12 +2,18 @@
 set -e
 . setdevkitpath.sh
 
-# build libapi19.a
+# patch toolchain, build libapi19, copy
 if [ ! -e ./api19/libapi19.a ];
 then
-cd api19
-find . -name "*.cpp" -type f | xargs $CXX -c -I./include
+cd api19/include-patch
+find . -name "*.h" -type f | sed 's#./##g' | xargs "cat {} >> $ANDROID_INCLUDE/{}"
+cd ..
+find . -name "*.cpp" -type f | xargs $CXX -c
 $AR rc ./libapi19.a *.o
+# well, maybe someday there won't be just one folder in setdevkitpath.sh
+# also copy to toolchain (?)
+LDFLAGS="$LDFLAGS $TOOLCHAIN/sysroot/usr/lib"
+echo "$LDFLAGS" | sed 's#-L##g' | xargs -n 1 cp libapi19.a
 cd ..
 fi
 
