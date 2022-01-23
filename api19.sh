00a361c5cd4b8bff89a2c86342a71ac320e724e1
Google61 <golubev634@gmail.com>
2022-01-23 13:43:41 +0300

    Update api19.sh

--- api19.sh
+++ api19.sh
@@ -6,7 +6,7 @@ set -e
 if [ ! -e ./api19/libapi19.a ];
 then
 cd api19/include-patch
-find . -name "*.h" -type f | sed 's#./##g' | xargs -i  sh -c 'cat {} >> "$ANDROID_INCLUDE"/{} && cat $ANDROID_INCLUDE/{}'
+find . -name "*.h" -type f | sed 's#./##g' | xargs -i  sh -c 'cat {} >> "$ANDROID_INCLUDE"/{}'
 cd ..
 find . -name "*.cpp" -type f | xargs $CXX -c
 $AR rc ./libapi19.a *.o
