7a4f9437f68b401e35cfb4ba3dc652ca1904c275
Google61 <golubev634@gmail.com>
2022-01-25 23:23:34 +0300

    Update api19.sh

--- api19.sh
+++ api19.sh
@@ -3,26 +3,26 @@ set -e
 . setdevkitpath.sh
 
 # patch toolchain, build libapi19, copy
-if [ ! -e ./api19/libapi19.a ];
-then
+#if [ ! -e ./api19/libapi19.a ];
+#then
 cd api19/include-patch
 echo "$0: Patching includes"
 find . -name "*.h" -type f | sed 's#./##g' | xargs -i  sh -c 'cat {} >> "$ANDROID_INCLUDE"/{}'
 cd ../include
 cp -rf * "$ANDROID_INCLUDE"/
 cd ..
-echo "$0: Compiling sources"
-$CXX -shared -I. -o libapi19.so *.cpp arch-$TARGET_JDK/syscalls/*.S
+#echo "$0: Compiling sources"
+#$CXX -shared -I. -o libapi19.so *.cpp arch-$TARGET_JDK/syscalls/*.S
 #echo "$0: Building library"
 #$AR rcv ./libapi19.a *.o
 cd ..
-fi
+#fi
 
 # patch sources
-if [ -d ./openjdk ];
-then
-cd ./openjdk
+#if [ -d ./openjdk ];
+#then
+#cd ./openjdk
 #echo "$0: Patching OpenJDK makefiles"
 #find . -name "*.gmk" -type f | xargs sed -i 's/(LIBDL)/(LIBDL) -lapi19/g'
-cd ..
-fi
+#cd ..
+#fi
