761f4f0b71575df748376da1c98384e495808784
Google61 <golubev634@gmail.com>
2022-01-26 22:51:59 +0300

    Update fcntl.h

--- api19/include-patch/fcntl.h
+++ api19/include-patch/fcntl.h
@@ -1,8 +1,8 @@
 __BEGIN_DECLS
   
-int creat64(const char* __path, mode_t __mode);
-int openat64(int __dir_fd, const char* __path, int __flags, ...);
-int open64(const char* __path, int __flags, ...);
+int open64(const char* __path, int __flags, ...) {
+  open(__path, __flags, ...);
+}
 
 int fallocate(int __fd, int __mode, off_t __offset, off_t __length) __RENAME_IF_FILE_OFFSET64(fallocate64);
 int fallocate64(int __fd, int __mode, off64_t __offset, off64_t __length);
