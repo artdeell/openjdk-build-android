e9574ecebae33deee0bf6c48fd15b4b405ba222f
Google61 <golubev634@gmail.com>
2022-02-10 12:55:34 +0300

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 if [ "$TARGET_JDK" == "arm" ]; then
-git clone --depth 1 https://github.com/PojavLauncherTeam/openjdk-aarch32-jdk8u openjdk
+git clone --depth 1 https://github.com/Google61/openjdk-aarch32-jdk8u openjdk
 elif [ "$BUILD_IOS" == "1" ]; then
 git clone --depth 1 --branch ios https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u openjdk
 else
