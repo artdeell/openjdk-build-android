55051bd8bf7aa1eb3d3fbc1151c4401433e12951
Google61 <golubev634@gmail.com>
2022-02-11 18:13:08 +0300

    Update signal_extra.cpp

--- api19/signal_extra.cpp
+++ api19/signal_extra.cpp
@@ -5,3 +5,31 @@ int pthread_sigmask(int how, const sigset_t* new_set, sigset_t* old_set) {
   ErrnoRestorer errno_restorer;
   return (sigprocmask(how, new_set, old_set) == -1) ? errno : 0;
 }
+template <typename SigSetT>
+int SigAddSet(SigSetT* set, int sig) {
+  int bit = sig - 1; // Signal numbers start at 1, but bit positions start at 0.
+  unsigned long* local_set = reinterpret_cast<unsigned long*>(set);
+  if (set == nullptr || bit < 0 || bit >= static_cast<int>(8*sizeof(*set))) {
+    errno = EINVAL;
+    return -1;
+  }
+  local_set[bit / LONG_BIT] |= 1UL << (bit % LONG_BIT);
+  return 0;
+}
+int sigaddset64(sigset64_t* set, int sig) {
+  return SigAddSet(set, sig);
+}
+template <typename SigSetT>
+int SigDelSet(SigSetT* set, int sig) {
+  int bit = sig - 1; // Signal numbers start at 1, but bit positions start at 0.
+  unsigned long* local_set = reinterpret_cast<unsigned long*>(set);
+  if (set == nullptr || bit < 0 || bit >= static_cast<int>(8*sizeof(*set))) {
+    errno = EINVAL;
+    return -1;
+  }
+  local_set[bit / LONG_BIT] &= ~(1UL << (bit % LONG_BIT));
+  return 0;
+}
+int sigdelset64(sigset64_t* set, int sig) {
+  return SigDelSet(set, sig);
+}
