0d5007ace5d77e6da22ec2dd4ade8c4bda93c454
Google61 <golubev634@gmail.com>
2022-02-11 08:05:40 +0300

    Update ndkdefs.h

--- api19/include-ndk/ndkdefs.h
+++ api19/include-ndk/ndkdefs.h
@@ -1,4 +1,8 @@
+/* ndkdefs.h: define unified headers macros for pre-unified headers sysroot */
+
 #define __INTRODUCED_IN(x)
+#define __INTRODUCED_IN_64(x)
+
 #define __ANDROID_API__ 19
 
 #include <android/api-level.h>
