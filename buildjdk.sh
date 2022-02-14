700ae82f89f62931194dac4032de9605e47d639e
Google61 <golubev634@gmail.com>
2022-02-14 07:33:09 +0300

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -60,7 +60,7 @@ cd api19
 cp -rf include-ndk/* "$ANDROID_INCLUDE"/
 $CXX -I./include -c -std=c++11 *.cpp
 $CC -I./include -c *.c
-$AS -I./include -c syscalls/*.S
+$CCNEW -I./include -c syscalls/*.S
 $AR rcv ./libapi19.a *.o syscalls/*.o
 #export CFLAGS+=" -D__ANDROID_API__=19"
 cd ..
