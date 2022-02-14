35dbef54f9a0bb7501f2ce251eb449bfe66ea988
Google61 <golubev634@gmail.com>
2022-02-14 17:50:23 +0300

    Update maketoolchain.sh

--- maketoolchain.sh
+++ maketoolchain.sh
@@ -11,7 +11,6 @@ $NDK/build/tools/make-standalone-toolchain.sh \
 cp devkit.info.${TARGET_SHORT} $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain/
 
 $ANDROID_NDK_HOME/build/tools/make-standalone-toolchain.sh \
-	--system=linux-x86_64 \
 	--arch=${TARGET_SHORT} \
 	--platform=android-$API \
 	--install-dir=$GITHUB_WORKSPACE/android-toolchain
